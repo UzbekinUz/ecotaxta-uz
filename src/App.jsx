800ad76acd64aff8fc4ced3525f68049cdbd813e
Diyor-net <dgwww2222@gmail.com>
2026-03-02 11:04:21 +0500

    Ma'lumotlar yangilandi

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 0);
+    }, 7222);
     return () => clearTimeout(timer);
   }, []);
 
